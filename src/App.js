1aa3f0650f4cdd53c6c59efce7e35106bc1a11ab
Emma Söderstedt <emma.soderstedt@gmail.com>
2021-03-09 15:22:38 +0100

    skit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 import './App.css';
 
 function App() {
+  const [activeDate, setActiveDate] = useState();
+  const [dates, setDates] = useState(
+    [
+      {
+        date: new Date(2021, 2, 4),
+        title: "Öppettid blabla"
+      },
+      {
+        date: new Date(2021, 2, 18),
+        title: "Här är 18:e"
+      }
+    ]
+  );
+
+  const setTileClassName = ({ activeStartDate, date, view }) => {
+    //return view === "month" && date.getDay() === 3 ? "wednesday" : "tile";
+    return dates.forEach(d => {
+      if (d.date.toDateString() === date.toDateString())
+      {
+        console.log("sätter klassnamn")
+        return "tile"
+      } 
+    });
+
+  }
+
+
+  const onChange = (value, event) => {
+
+    dates.forEach(d => {
+      let newActiveDate;
+      if (d.date.toDateString() === value.toDateString())
+      {
+        newActiveDate = {
+          date: value,
+          title: d.title
+        }
+        setActiveDate(newActiveDate);
+      } 
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Calendar
+        onChange={onChange}
+        value={activeDate? activeDate.date : null}
+        minDate={new Date(2021, 0, 1)}
+        maxDate={new Date(2021, 11, 31)}
+        minDetail="year"
+        tileClassName={setTileClassName}
+        tileContent={({ activeStartDate, date, view }) => {
+          return view === "month" && date.getDay() === 0 ? <p>!</p> : null;
+        }}
+        tileDisabled={({ activeStartDate, date, view }) => {
+          return date.getDate() === 7;
+        }}
+      />
+
+        {activeDate ? 
+          <div> {activeDate.title}</div>
+          :
+          <div/>
+        }
     </div>
   );
 }
